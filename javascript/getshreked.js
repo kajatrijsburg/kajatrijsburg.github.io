cb1c4bc1053baf82897ca4bf94913fcca6efa401
kajatrijsburg <kajatrijsburg@gmail.com>
2016-01-07 01:31:03 +0100

    adding some javascript

---
+++ javascript/getshreked.js
@@ -0,0 +1,8 @@
+unction get_shreked(){
+text_to_replace_id = document.getElementById("replace");
+input_id = document.getElementById("input");
+
+input = input_id.value;
+
+text_to_replace_id.innerHTML = "<br />I was only nine years old <br />I loved " + input + " so much, I owned all the movies and merchandise <br />I pray to " + input + " every night, thanking him for the life I have been given<br />\"" + input + " is love\", I say, \"" + input + " is life\"<br />My dad hears me and calls me a faggot<br />He is obviously jealous of my devotion to " + input + "<br />I called him a cunt<br />He slaps me and sends me to my room<br />I am crying now, because my face hurts<br />I go into my bed and it is very cold<br />I feel a warmth moving towards me<br />I feel something touch me<br />It's " + input + "<br />I am so happy<br />He whispers into my ear, \"This is my swamp\"<br />He grabs me with his powerful " + input + " hands and puts me on my hands and knees<br />I'm ready<br />I spread my ass cheeks for " + input + "<br />He penetrates my butthole<br />It hurts so much, but I do it for " + input + "<br />I can feel my anus tearing as my eyes start to water<br />I push against his force<br />I want to please " + input + "<br />He roars a mighty roar as he fills my butt with his love<br />My dad walks in<br />" + input + " looks him deep in the eyes and says, \"It's all " + input + " now\"<br />" + input + " leaves through my window<br />" + input + " is love, " + input + " is life"
+}
