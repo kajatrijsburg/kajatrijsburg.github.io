f5541d6ae22920404c7da60fdbf25ddbf7c86d95
kajatrijsburg <kajatrijsburg@gmail.com>
2016-01-11 15:05:54 +0100

    added a encrypt function

--- javascript/password.js
+++ javascript/password.js
@@ -1,5 +1,17 @@
-function decrypt(number, string){
+function decrypt(num, str){
   alert(number+string)
   for(i=0; i < number; i++){
   }
+}
+function encrypt(num, str){
+  alert(number+string)
+  for(i=0; i < number; i++){
+    var len = str.lenght;
+    var newString = ""
+    for(j=0; j < len; j++){
+      chr.fromCharCode( str.charCodeAt(0)+1);
+      newString = newString + chr
+    }
+  }
+  return newString;
 }
