2f3bad9353ba8d47d8bf3b4ad7266f481355d181
kajatrijsburg <kajatrijsburg@gmail.com>
2016-01-19 10:18:51 +0100

    uploaded newer, better version of loveletter.js

---
+++ javascript/loveletter.js
@@ -0,0 +1,125 @@
+var name = prompt("enter the name of your love interest","right here you dummy");
+
+var line = [];
+line[0]=["My dearest " + name, 
+	"My greatest love " + name, 
+	"the love of my life " + name,
+	"my precious " + name,
+	"My soul mate "+ name];
+				
+line[1]=["You might not know me but, ",
+	"This might be a surprise, ",
+	"I've admired you from a distance and ",
+	"I think that "];
+
+line[2]=["I've fallen in love with you. ",
+	"you're my one and only love. ",
+	"my heart beats faster whenever I see you. ",
+	"you're the source of all my happiness. "];
+				
+line[3]=["The best thing about you is your ",
+	"I just so admire your ",
+	"I can't resist your ",
+	"You've caught me with your "];
+
+line[4]=["beauty. ",
+	"superior intellect. ",
+	"mystique. ",
+	"defiant spirit. "];
+
+line[5]=["I fantasize every day about ",
+	"I often dream of ",
+	"we should try ",
+	"I could really see us "];
+	
+line[6]=["running away together. ",
+	"Getting wed, having a few kids, taking some pictures, retiring to the south of France, and dying. ",
+	"experiencing a night wilder that even our wildest dreams. ",
+	"doing something fun with you. "]
+
+line[7]=["Oh yes, "+name+", ",
+	"Indeed my love, ",
+	"That's why ",
+	"I truly believe that ", ];
+
+line[8]=["We would make the perfect couple. ",
+	"we are star-crossed lovers. ",
+	"I'll love you for ever and ever and ever and ever.",
+	"I'll never grow tired of your nagging"]
+	
+line[9]=["A day without you ",
+	"Every night I can't feel you in my arms ",
+	"When another second without you passes, it ",
+	"I'm so in love that being without you "];
+	
+line[10]=["feels like the world is ending. ",
+	"seems like an eternity in the deepest depths of hell. ",
+	"would be worse than anything I could possibly imagine. ",
+	"makes me feel so alone that I could not survive it for long. "];
+	
+line[11]=["I beg you please, ",
+	"I truly hope that ",
+	"maybe ",
+	"I would sell my soul and all I've ever possessed so that "];
+	
+line[12]=["we could be together forever. ",
+	"the two of us should never be apart. ",
+	"we will have wonderful children who will look just like you. ",
+	"nothing could ever stop us from finding each other. "],
+
+line[13]=["What I admire most about you is ",
+	"The reason why I love you so is ",
+	"You know what's so great about you? of course you do it's ",
+	"What the greatest thing about you is "];
+	
+line[14]=["that you don't even know how pretty you are. ",
+	"the simple fact that you're so sexy. ",
+	"the way you say \"Octagon.\" ",
+	"your perfectly symmetrical face. "],
+
+line[15]=["I can feel myself falling in love with you ",
+	"The moments when I feel in love the most are ",
+	"I just can't help loving you ",
+	"I feel like Satan himself is talking to me "],
+
+line[16]=["when you do that thing with your hair. ",
+	"every time you say my name like that. ",
+	"when the police tells me that I can't go to your neighbourhood any more. ",
+	"whenever I hear that voice in my that tells me I should take more pictures of you. "],
+
+line[17]=["I promise to never ",
+	"I definitely totally wouldn't ",
+	"Nothing could tear us apart, I wouldn't ever ",
+	"Even if the gods themselves would try to separate us, I would never "]
+	
+line[18]=["let my LoL addiction come between us.",
+	"eat all the chocolate I meant to give you. ",
+	"betray us. ",
+	"be a bad, bad, bad kitten. ",
+	"watch netflix without you. ",
+	"lick your toes at night. ",
+	"never gonna give you up, \nnever gonna let you down, \nnever gonna run around and desert you, \nnever gonna make you cry, \nnever gonna say goodbye, \nnever gonna tell a lie and hurt you"];
+	
+line[19]=["So I guess I wanted to know. ",
+	"I can't hold back any more, I just have to ask you... ",
+	"erm, sooooo...., eh... ",
+	"I feel like a caged bird, I need to know. ",
+	"I must ask you..."];
+	
+line[20]=["Do you love me like I do?",
+	"Do you want to go to morgue with me?",
+	"Are you in secretly with Morgan Freeman?",
+	"Are you as in love with my Runescape avatar as you should be?",
+	"Do you love me?",
+	"Do you worship the one and only god, like a true follower of the morning star?"];
+	
+var letter = "To: \n";
+
+for(i=0; i<line.length; i++){
+	letter = letter + line[i][Math.round(Math.random()*line[i].length)];
+	if(i%2 == 0){
+		letter = letter + "\n"
+	}
+}
+
+alert(letter);
